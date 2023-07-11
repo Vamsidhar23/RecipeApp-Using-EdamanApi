803892d5a5ba5c226bad7caa27ecebe18c583d0c
Gokavarapu Vamsidhar <125563361+Vamsidhar23@users.noreply.github.com>
2023-07-11 21:15:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { useState } from 'react';
+import Axios from 'axios'
+import Products from "./Products"
+
+function App() {
+
+  const [search,setsearch] = useState("");
+  const [data,setdata] =useState([]);
+
+  const your_app_id = "39567d20";
+  const your_app_key = "275db54bcba574dd7f00486d7da5acad"; 
+
+  var url = `https://api.edamam.com/api/recipes/v2?type=public&q=${search}&app_id=${your_app_id}&app_key=${your_app_key}`
+
+  async function getRecipes(){
+    var result = await Axios.get(url);
+    setdata(result.data.hits);
+    console.log(result);
+  }
+
+  function handleClick(e){
+    getRecipes();
+    e.preventDefault();
+  }
+
+
+  return (
+    <div className="App">
+        <center>
+          <h1>🍔 Food Recipe App 🍔</h1>
+          <form onSubmit={handleClick}>
+            <input className='input__name' type='text' value = {search} onChange={(e) => setsearch(e.target.value)}></input><br/>
+            <input className='btn btn-primary' type='submit' value='Search'></input>
+          </form>
+          {data.length>= 1 ? <Products data={data}/> : null}
+        </center>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Products.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Products({data}) {
+    return (
+        <div>
+            <div className="row">
+                {data.map(data =>  
+                <div className="col-md-4">
+                    <div class="card" style={{"width": "18rem"}}>
+                    <img class="card-img-top" src={data.recipe.image} alt="Card image" />
+                    <div class="card-body">
+                    <center>
+                        <h5 class="card-title">{data.recipe.label}</h5>
+                        <p class="card-text">Total Amount Of Calories : {Math.round(data.recipe.calories)}</p>
+                        <a href={data.recipe.shareAs} class="btn btn-primary">Buy</a>
+                        </center>
+                    </div>
+                    </div>
+                </div>
+                )}
+                <footer className='text-center mt-1 py-3 custom-footer'>Made by Vamsidhar Gokavarapu</footer>
+            </div>
+        </div>
+    )
+}
+export default Products
